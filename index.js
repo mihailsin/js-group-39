9cee16502577138b4dd700bce71257dbcf52fd80
Mihail Sinkov <mihailsin88@gmail.com>
2021-09-23 19:15:26 +0300

    changed 07-lesson

--- index.js
+++ index.js
@@ -5,6 +5,14 @@
 // logProduct(product) - коллбек принимающий обьект продукта и логирующий его в консоль
 // logTotalPrice(product) - коллбек принимающий обьект продукта и логирующий общую стоимость товара в консоль
 
+// const createProduct = (obj, callback) => {
+//   const product = { ...obj, id: Date.now() };
+//   callback(product);
+// };
+// const logProduct = product => console.log('product', product);
+
+// const logTotalPrice = product => console.log('price', product.price * product.quantity);
+
 // createProduct({ name: '🍎', price: 30, quantity: 3 }, logProduct);
 // createProduct({ name: '🍋', price: 20, quantity: 5 }, logTotalPrice);
 
@@ -12,20 +20,28 @@
 //   а вторым - функцию, которая применится к каждому элементу массива.Функция each должна вернуть
 //   новый массив, элементами которого будут результаты вызова коллбека.
 
+// const each = (array, callback) => {
+//   const newArray = [];
+//   for (const element of array) {
+//     newArray.push(callback(element));
+//   }
+//   return newArray;
+// };
+
 // console.log(
 //   each([64, 49, 36, 25, 16], function (value) {
 //     return value * 2;
-//   })
+//   }),
 // );
 // console.log(
 //   each([64, 49, 36, 25, 16], function (value) {
 //     return value - 10;
-//   })
+//   }),
 // );
 // console.log(
 //   each([64, 49, 36, 25, 16], function (value) {
 //     return Math.sqrt(value);
-//   })
+//   }),
 // );
 // console.log(
 //   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
@@ -40,10 +56,10 @@
 
 // Выполните рефакторинг кода используя стрелочные функции.
 
-// function createProduct(partialProduct, callback) {
+// const createProduct = (partialProduct, callback) => {
 //   const product = { id: Date.now(), ...partialProduct };
 //   callback(product);
-// }
+// };
 
 // function logProduct(product) {
 //   console.log(product);
@@ -58,77 +74,75 @@
 
 // Выполните рефакторинг кода используя стрелочные функции.
 
-// function each(array, callback) {
+// const each = (array, callback) => {
 //   const newArr = [];
 //   for (const el of array) {
 //     newArr.push(callback(el));
 //   }
 //   return newArr;
-// }
+// };
 
 // console.log(
 //   each([64, 49, 36, 25, 16], function (value) {
 //     return value * 2;
-//   })
+//   }),
 // );
 // console.log(
 //   each([64, 49, 36, 25, 16], function (value) {
 //     return value - 10;
-//   })
+//   }),
 // );
 // console.log(
 //   each([64, 49, 36, 25, 16], function (value) {
 //     return Math.sqrt(value);
-//   })
+//   }),
 // );
 // console.log(
 //   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
 //     return Math.ceil(value);
-//   })
+//   }),
 // );
 // console.log(
 //   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
 //     return Math.floor(value);
-//   })
+//   }),
 // );
 
 // Выполните рефакторинг кода используя метод forEach и стрелочные функции.
 
-// function logItems(items) {
+// const logItems = items => {
 //   console.log(items);
-//   for (let i = 0; i < items.length; i += 1) {
-//     console.log(`${i + 1} - ${items[i]}`);
-//   }
-// }
+//   items.forEach((item, index) => console.log(item, index));
+// };
 
-// logItems(["Mango", "Poly", "Ajax"]);
-// logItems(["🍎", "🍇", "🍑", "🍌", "🍋"]);
+// logItems(['Mango', 'Poly', 'Ajax']);
+// logItems(['🍎', '🍇', '🍑', '🍌', '🍋']);
 
 // Выполните рефакторинг кода используя метод forEach и стрелочные функции.
 
-// function printContactsInfo({ names, phones }) {
-//   const nameList = names.split(",");
-//   const phoneList = phones.split(",");
-//   for (let i = 0; i < nameList.length; i += 1) {
-//     console.log(`${nameList[i]}: ${phoneList[i]}`);
-//   }
-// }
+// const printContactsInfo = ({ names, phones }) => {
+//   const nameList = names.split(',');
+//   const phoneList = phones.split(',');
+//   nameList.forEach((name, index) => {
+//     console.log(name, phoneList[index]);
+//   });
+// };
 
 // printContactsInfo({
-//   names: "Jacob,William,Solomon,Artemis",
-//   phones: "89001234567,89001112233,890055566377,890055566300",
+//   names: 'Jacob,William,Solomon,Artemis',
+//   phones: '89001234567,89001112233,890055566377,890055566300',
 // });
 
 // Выполните рефакторинг кода используя метод forEach и стрелочные функции.
 
-// function calсulateAverage(...args) {
-//   let total = 0;
-//   for (let i = 0; i < args.length; i++) {
-//     total += args[i];
-//   }
-//   return total / args.length;
-// }
+const calсulateAverage = (...args) => {
+  let total = 0;
+  [...args].forEach(arg => {
+    total += arg;
+  });
+  return total / args.length;
+};
 
-// console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
-// console.log(calсulateAverage(14, 8, 2)); // 8
-// console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
+console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
+console.log(calсulateAverage(14, 8, 2)); // 8
+console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
